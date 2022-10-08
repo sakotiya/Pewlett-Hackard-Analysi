f55d66e98936562adb5204eb614ffb00da3cefe0
sakotiya <sakotiya2707@gmail.com>
2022-10-07 19:02:38 -0700

    Add query

--- Query/PH-EmployeesDB.sql
+++ Query/PH-EmployeesDB.sql
@@ -101,3 +101,65 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 select * from retirement_info;
+
+drop table retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+select d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+from retirement_info left join 
+dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+ 
+ 
+ 
+ -- Joining retirement_info and dept_emp tables
+SELECT re.emp_no,
+    re.first_name,
+re.last_name,
+    de.to_date
+FROM retirement_info as re
+LEFT JOIN dept_emp as de
+ON re.emp_no = de.emp_no;
+
+
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+de.to_date
+into current_emp
+from retirement_info as ri   
+Left  JOIN dept_emp as de 
+ON ri.emp_no = de.emp_no
+where de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
